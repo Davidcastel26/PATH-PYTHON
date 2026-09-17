0607be1cef3afd49343c4b376bf3bf5eb8aabe2e
Davidcastel26 <davcastellanoslarios02@gmail.com>
2026-09-17 17:34:52 -0600

    feat(BankAccount): implement BankAccount class with interest rate management

---
+++ funtamentos-01/08-poo_a/08-class_static_example.py
@@ -0,0 +1,11 @@
+
+class BankAccount:
+    interest_rate = 0.02
+    
+    def __init__(self, holder, balance):
+        self.holder = holder
+        self.balance = balance
+    
+    @classmethod
+    def change_interest_rate(cls, new_rate):
+        cls.interest_rate = new_rate
