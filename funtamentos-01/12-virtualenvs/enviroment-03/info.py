6dc2c22385be21df652bbdd45eb85162e56a6631
Davidcastel26 <davcastellanoslarios02@gmail.com>
2026-09-21 12:19:05 -0600

    Add pyproject.toml for project configuration and dependencies

---
+++ funtamentos-01/12-virtualenvs/enviroment-03/info.py
@@ -0,0 +1,16 @@
+# puedes entrar al Virtual Environment puedes usar:
+# Mac/Linux
+# pip3 install poetry
+
+# eval "$(poetry env info --path)/bin/activate"
+
+# poetry add --package--
+
+# Windows
+# & ((poetry env info --path) + "\Scripts\Activate.ps1")
+# o
+# $envPath = poetry env info --path
+# & "$envPath\Scripts\Activate.ps1"
+
+# Otra forma es correr directamente el programa sin entrar al entorno
+# poetry run python script.py
