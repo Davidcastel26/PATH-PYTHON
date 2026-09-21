c4d346722fd246bb288b13674b1a3462e2a633de
Davidcastel26 <davcastellanoslarios02@gmail.com>
2026-09-21 10:23:04 -0600

    feat(modules): add 04-alias_and_import.py with math and datetime imports

---
+++ funtamentos-01/11-modules/04-alieas_and_import.py
@@ -0,0 +1,12 @@
+# import full lib
+import math
+# import function specific
+from datetime import datetime
+# import wiht alias
+import random as ran
+# multiple imports 
+from math import sin, cos, pi, sqrt
+
+print(math.sqrt(12))
+print(datetime.now())
+print(ran.randint(1,3))
